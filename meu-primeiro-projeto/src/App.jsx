7119feb0ec6aa15430cdda888a695fd29f6b28ee
Liillyan <lillyan1995@outlook.com>
2022-12-13 12:18:01 -0300

    Atualizando App com as alterações feitas durante a aula

--- meu-primeiro-projeto/src/App.jsx
+++ meu-primeiro-projeto/src/App.jsx
@@ -1,17 +1,15 @@
 import './App.css'
-import Info from './components/Info'
-import Texto from './components/texto'
+import Relogio from './components/Relogio'
+import Formulario from './components/Formulario'
 
 function App() {
 
   return (
+    
     <div>
-    <h1>Componentes do Instagran</h1>
-    <Info quantidade="123" tipo="publicações"/>
-    <Info quantidade="10.412" tipo="seguidores"/>
-    <Info quantidade="49" tipo="seguindo"/>
-    <Texto> atributo 1</Texto>
-    <Texto> Atributo 2</Texto>
+      <h1> Aula 05 </h1>
+      <Relogio />
+      <Formulario />
     </div>
   )
 }
