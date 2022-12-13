c55afb23a1ce28ee9483456defaf1f9f7444779b
Liillyan <lillyan1995@outlook.com>
2022-12-13 19:34:55 -0300

    Subindo 3º projeto com REACT

---
+++ meu-terceiro-projeto/src/data/profile-data.js
@@ -0,0 +1,6 @@
+export const information {
+    nome: "Lillyan";
+    apelido: "Lilly";
+    foto: "https://avatars.githubusercontent.com/u/103754183?v=4";
+    descrição: "Projeto 3 criado durante a 6ª aula da PretaLab, em REACT"
+};
