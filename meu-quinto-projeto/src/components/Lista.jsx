4cbd4f7d8eb2e3ecc9c657c785a31fd5dad9ab56
Liillyan <lillyan1995@outlook.com>
2022-12-15 18:15:53 -0300

    Atualizações feitas na aula 9

--- meu-quinto-projeto/src/components/Lista.jsx
+++ meu-quinto-projeto/src/components/Lista.jsx
@@ -2,7 +2,10 @@ import { useEffect, useState} from "react"
 import axios from "axios"
 
 export const Lista = () => {
+
     const [pokemons, setPokemons]= useState([])
+    const [listaOculta, setListaOculta] = useState(false)
+
     useEffect(() => {
         async function pegaDados() {
         const resposta = await axios.get('https://pokeapi.co/api/v2/pokemon')
@@ -11,15 +14,22 @@ export const Lista = () => {
         pegaDados()
     }, [])
 
+    function ocultarLista() {
+        setListaOculta((valor) => !valor);
+        console.log(listaOculta)
+    }
+
     return(
         <>
+        <h2>Pokemon</h2>
+        <input type='checkbox' onChange={ocultarLista}/>
+        <label> Ocultar Lista </label>
         <p> {pokemons.map(pokemon => {
             return(
                 <p key={pokemon.name}>{pokemon.name}</p>
             )
         })} </p>
-        <p> Item 2 </p>
-        <p> Item 3 </p>
+        
         </>
     )
 }
